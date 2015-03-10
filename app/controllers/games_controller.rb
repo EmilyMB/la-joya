121c977053e9a52f9094e2ed414d23d3e3099573
Emily Berkeley <eberkeley@gmail.com>
2015-03-10 14:52:10 -0600

    Add quiz functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,20 @@ class GamesController < ApplicationController
   def new
     words = Upload.all.shuffle
     @word = words.first
-    @word_list = words[0..3]
+    @word_list = words[0..3].shuffle
+    session[:word] = @word
+    session[:word_list] = @word_list
+  end
+
+  def update
+    # binding.pry
+    if params[:guess].to_i == session[:word]["id"]
+      redirect_to new_game_path
+    else
+      render :new
+    end
+  end
+
+  def check
   end
 end
