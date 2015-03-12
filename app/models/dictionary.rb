57fae0c18e6a963446d2480f186dee68be73670a
Emily Berkeley <eberkeley@gmail.com>
2015-03-12 16:02:56 -0600

    Cleanup JS and change cancel action to delete upload

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -6,8 +6,4 @@ class Dictionary
   def self.find(word)
     service.dictionary(word)
   end
-
-  # def self.update(id, params)
-  #   service.update_school(id, params)
-  # end
 end
--- app/services/dictionary_service.rb
+++ app/services/dictionary_service.rb
@@ -2,24 +2,18 @@ class DictionaryService
   attr_reader :connection
 
   def initialize
-    @connection = Faraday.new(url: "https://api.pearson.com:443/v2/dictionaries/lase")
+    @connection = Faraday.new(url:
+      "https://api.pearson.com:443/v2/dictionaries/lase")
   end
 
   def dictionary(word)
     parse(connection.get("entries?headword=#{word}"))
   end
 
-  # def school(id)
-  #   parse(connection.get("schools/#{id}"))
-  # end
-  #
-  # def destroy_school(id)
-  #   connection.delete("schools/#{id}")
-  # end
-
   private
 
   def parse(response)
-    JSON.parse(response.body)["results"][0]["senses"][0]["translations"][0]["text"][0]
+    JSON.parse(response.body)["results"][0]["senses"][0]
+                             ["translations"][0]["text"][0]
   end
 end
