ac0e75dd940cc09fbfbc5aaec28ea653da970d8c
Emily Berkeley <eberkeley@gmail.com>
2015-03-13 12:13:50 -0600

    remove validation

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,7 +2,7 @@ class Upload < ActiveRecord::Base
   belongs_to :user
   validates :url, presence: true
   validates :meaning, presence: true
-  validates :user_id, presence: true
+  # validates :user_id, presence: true
 
   def self.with_meaning
     where.not(meaning: ["no meaning", ""])
